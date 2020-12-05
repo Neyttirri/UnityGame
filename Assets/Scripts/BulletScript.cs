2e963431c22ced2ccdb75422f9183ddd4543d23b
Elena Tabakova <elena.tabakova@nexnet.de>
2020-12-05 20:53:28 +0100

    fix bullets going through walls
    - collision detection works only when at least one oject has a non-kinematic Rigidbody -> bullet and watterBall are dynamic now, with all variables set to 0 (or ~ 0)

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+    private void OnCollisionEnter2D(Collision2D collision)
  	{
 		if (collision.gameObject.CompareTag("Wall")) 
      	{
--- Assets/Scripts/waterBall.cs
+++ Assets/Scripts/waterBall.cs
@@ -18,8 +18,11 @@ public class waterBall : MonoBehaviour
 		Destroy(this.gameObject, 2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    private void OnCollisionEnter2D(Collision2D collision)
+ 	{
+		if (collision.gameObject.CompareTag("Wall")) 
+     	{
+       	 	Destroy(gameObject);
+       	}
+ 	}
 }
